dbb4b0e17b52c47f9c49680db0876c165efbb28b
Eldargv <drevniyrus@yandex.ru>
2021-10-21 18:39:18 +0300

    fixing bugs

--- bot/bot.py
+++ bot/bot.py
@@ -165,7 +165,7 @@ async def motivation_words(message: types.Message):
     if 12 <= datetime.now().hour <= 20:
         cursor = conn.cursor()
         user_id = message.from_user.id
-        cursor.execute(f"UPDATE Users SET reports = true WHERE user_handle = '{user_id}'")
+        cursor.execute(f"UPDATE Users SET reports = true WHERE user_id = '{user_id}'")
         cursor.close()
         await message.reply(random.choice(motivation) + ', ' + message.from_user.first_name + '!')
 
