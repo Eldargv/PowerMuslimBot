8e4497ddb8580f4d2d1d46544d1730826e1e3a99
Eldargv <drevniyrus@yandex.ru>
2021-10-22 15:27:16 +0300

    code refactoring

--- bot/bot.py
+++ bot/bot.py
@@ -35,20 +35,6 @@ sticker_demotivation = [
 ]
 
 
-class NotDigit(Exception):
-    pass
-
-
-class IncorrectSurah(Exception):
-    pass
-
-
-class IncorrectAyah(Exception):
-    pass
-
-class InCorrectInput(Exception):
-    pass
-
 def correct(msg):
     explanation_index = msg.find('***')
     if (explanation_index > -1):
@@ -63,6 +49,26 @@ def correct(msg):
                 msg = msg[:i + 1]
                 break
     return msg
+
+
+def get_ayah_by_num(surah_ayah):
+    if len(surah_ayah) != 2:
+        return "Неправильный формат. Вы можете ввести номер суры и аята через пробел, запятую и двоеточие"
+    surah, ayah = surah_ayah
+    if not surah.isdigit() or not ayah.isdigit():
+        return "Вы ввели не число"
+    print("nums got", surah, ayah)
+    if 0 < int(surah) < 115:
+        verses = Quran[surah].items()
+        print("trying get ayah")
+        for n, t in verses:
+            nums = list(map(int, re.split('[-:,]', n)))
+            if nums[1] <= int(ayah) <= nums[-1]:
+                print("Find!")
+                return correct(n + t)
+        return "Неверный номер аята"
+    else:
+        return "Неверный номер суры"
 
 
 @dp.message_handler(commands="start")
@@ -81,39 +87,7 @@ async def get_random_verse(message: types.Message):
 @dp.message_handler(chat_type=types.ChatType.PRIVATE)
 async def get_specific_verse(message: types.Message):
     surah_ayah = re.split(', | |:|,', message.text)
-    msg = ""
-    try:
-        if len(surah_ayah) != 2:
-            raise InCorrectInput
-        surah, ayah = surah_ayah
-        if not surah.isdigit() or not ayah.isdigit():
-            raise NotDigit
-        print("nums got", surah, ayah)
-        if 0 < int(surah) < 115:
-            verses = Quran[surah].items()
-            print("trying get ayah")
-            for n, t in verses:
-                nums = list(map(int, re.split('[-:,]', n)))
-                if nums[1] <= int(ayah) <= nums[-1]:
-                    print("Find!")
-                    msg = n + t
-                    break
-            if len(msg) == 0:
-                raise IncorrectAyah
-        else:
-            raise IncorrectSurah
-    except NotDigit:
-        msg = "Вы ввели не число"
-    except IncorrectSurah:
-        msg = "Неверный номер суры"
-    except IncorrectAyah:
-        msg = "Неверный номер аята"
-    except InCorrectInput:
-        msg = "Неправильный формат. Вы можете ввести номер суры и аята через пробел, запятую и двоеточие"
-    except Exception as ex:
-        print("Something goes wrong!!!")
-        print(ex)
-    await message.answer(correct(msg))
+    await message.answer(get_ayah_by_num(surah_ayah))
 
 
 @dp.message_handler(filters.Text(startswith='@PowerMuslimBot'))
@@ -121,39 +95,7 @@ async def get_specific_verse(message: types.Message):
     ftext = message.text.replace('@PowerMuslimBot ', '')
     print(ftext)
     surah_ayah = re.split(', | |:|,', ftext)
-    msg = ""
-    try:
-        if len(surah_ayah) != 2:
-            raise InCorrectInput
-        surah, ayah = surah_ayah
-        if not surah.isdigit() or not ayah.isdigit():
-            raise NotDigit
-        print("nums got", surah, ayah)
-        if 0 < int(surah) < 115:
-            verses = Quran[surah].items()
-            print("trying get ayah")
-            for n, t in verses:
-                nums = list(map(int, re.split('[-:,]', n)))
-                if nums[1] <= int(ayah) <= nums[-1]:
-                    print("Find!")
-                    msg = n + t
-                    break
-            if len(msg) == 0:
-                raise IncorrectAyah
-        else:
-            raise IncorrectSurah
-    except NotDigit:
-        msg = "Вы ввели не число"
-    except IncorrectSurah:
-        msg = "Неверный номер суры"
-    except IncorrectAyah:
-        msg = "Неверный номер аята"
-    except InCorrectInput:
-        msg = "Неправильный формат. Вы можете ввести номер суры и аята через пробел, запятую и двоеточие"
-    except Exception as ex:
-        print("Something goes wrong!!!")
-        print(ex)
-    await message.answer(correct(msg))
+    await message.answer(get_ayah_by_num(surah_ayah))
 
 
 @dp.message_handler(commands="register", chat_type=('group', 'supergroup'))
@@ -193,7 +135,7 @@ async def chat_member_handler(update: types.ChatMemberUpdated):
         cursor.execute(f'INSERT INTO Chats VALUES ({chat_id}) ON CONFLICT DO NOTHING')
         cursor.close()
         await update.bot.send_message(chat_id,
-                                                "Всем ас-саляму алейкум! Отныне я незаконно забираю власть в руки и буду менеджить ваши отчеты! "
+                                                "Ас-саляму алейкум! Я буду менеджить ваши ежедневные отчеты! "
                                                 "\nОтчеты принимаются с 15:00 до 00:00 по мск. В 22:00 я тегну всех, кто не сдал отчеты к этому времени."
                                                 " Чтобы зарегистрироваться, отправьте /register."
                                                 "\n\nТакже я каждый день в 21:00 буду присылать подборку из трех случайно выбранных аятов."
@@ -232,35 +174,7 @@ async def morning_motivation():
         await bot.send_message(id, "Ну что, ребята, топим педаль газа в пол! Идем к божественным подаркам в новом дне!")
 
 
-async def reports_checker():
-    cursor = conn.cursor()
-    # Забираем из таблицы все id чатов-групп
-    cursor.execute('SELECT chat_id FROM Chats')
-    # Строим массив
-    chat_list = [int(chat_id[0]) for chat_id in cursor.fetchall()]
-    # Забираем из второй таблицы юзеров
-    cursor.execute('SELECT user_id, user_name, chat_id FROM Users WHERE reports = false')
-    # Кладем в массив
-    users = cursor.fetchall()
-    cursor.close()
-    # Начинаме собирать сообщения с индексами юзеров
-    messages = {}
-    # Строго задаем словарь
-    for chat_id in chat_list:
-        messages[chat_id] = ""
-    # Собираем сообщения с id юзеров
-    for user_id, user_name, chat_id in users:
-        messages[int(chat_id)] += f"[{user_name}](tg://user?id={user_id}), "
-    # Собираем для каждого чата сообщение с теми, кто не прислал отчет
-    for chat_id, message in messages.items():
-        if len(message) == 0:
-            await bot.send_message(int(chat_id), "Молодцы, ребята! Сегодня все прислали отчеты!")
-        else:
-            await bot.send_message(int(chat_id), "Не понял, а где отчеты от " + message[:-2] + " ...", parse_mode='Markdown')
-            await bot.send_sticker(int(chat_id), random.choice(sticker_demotivation))
-
-
-async def reports_cleaner():
+async def reports_checker(clean=False):
     cursor = conn.cursor()
     # Забираем из таблицы все id чатов-групп
     cursor.execute('SELECT chat_id FROM Chats')
@@ -280,12 +194,16 @@ async def reports_cleaner():
         messages[int(chat_id)] += f"[{user_name}](tg://user?id={user_id}), "
     # Собираем для каждого чата сообщение с теми, кто не прислал отчет
     for chat_id, message in messages.items():
-        cursor.execute(f'UPDATE Users SET reports = false WHERE chat_id = {int(chat_id)}')
         if len(message) == 0:
             await bot.send_message(int(chat_id), "Молодцы, ребята! Сегодня все прислали отчеты!")
         else:
-            await bot.send_message(int(chat_id), "Так и не дождался отчётов от " + message[:-2] + " (((", parse_mode='Markdown')
+            if clean:
+                await bot.send_message(int(chat_id), "Так и не дождался отчётов от " + message[:-2] + " (((", parse_mode='Markdown')
+            else:
+                await bot.send_message(int(chat_id), "Не понял, а где отчеты от " + message[:-2] + " ...", parse_mode='Markdown')
             await bot.send_sticker(int(chat_id), random.choice(sticker_demotivation))
+        if clean:
+            cursor.execute(f'UPDATE Users SET reports = false WHERE chat_id = {int(chat_id)}')
     cursor.close()
 
 
@@ -297,7 +215,7 @@ async def scheduler():
     aioschedule.every().day.at("18:00").do(time_send)
     aioschedule.every().day.at("3:00").do(morning_motivation)
     aioschedule.every().day.at("19:00").do(reports_checker)
-    aioschedule.every().day.at("21:00").do(reports_cleaner)
+    aioschedule.every().day.at("21:00").do(reports_checker, clean=True)
     while True:
         await aioschedule.run_pending()
         await asyncio.sleep(1)
@@ -319,7 +237,8 @@ async def set_default_commands():
 
 async def on_startup(dp):
     logging.warning(
-        'Starting connection. ')
+        'Starting connection. '
+    )
     asyncio.create_task(scheduler())
     await set_default_commands()
     await bot.set_webhook(WEBHOOK_URL,drop_pending_updates=True)
