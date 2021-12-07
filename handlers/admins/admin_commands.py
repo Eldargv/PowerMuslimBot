30583d4951dc6e2f83aa4aabfab5be8c03f1e53b
Eldargv <drevniyrus@yandex.ru>
2021-12-07 21:25:17 +0300

    removed from admin commands

--- handlers/admins/admin_commands.py
+++ handlers/admins/admin_commands.py
@@ -5,8 +5,3 @@ from loader import dp
 @dp.message_handler(chat_type="private", commands="send_message", user_id=916354662)
 async def new_user_start(message: types.Message):
     await dp.bot.send_message(-1001444174142, message.text.replace("/send_message", ''))
-
-
-@dp.message_handler(chat_type=('group', 'supergroup'), is_reply=True)
-async def new_user_start(message: types.Message):
-    await dp.bot.forward_message(916354662, message.chat.id, message.message_id)
