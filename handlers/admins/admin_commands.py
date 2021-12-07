b3635122850e2da3c9f378db1fe53c783f986890
Eldargv <drevniyrus@yandex.ru>
2021-12-07 21:30:58 +0300

    removed from admin commands

--- handlers/admins/admin_commands.py
+++ handlers/admins/admin_commands.py
@@ -1,7 +1,2 @@
 from aiogram import types
 from loader import dp
-
-
-@dp.message_handler(chat_type="private", commands="send_message", user_id=916354662)
-async def new_user_start(message: types.Message):
-    await dp.bot.send_message(-1001444174142, message.text.replace("/send_message", ''))
